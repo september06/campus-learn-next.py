5f5af27c02846fe4cdb334a2025e00b26c26a627
amar amer <september06@users.noreply.github.com>
2020-04-20 22:38:57 +0300

    Create questions 1.2.6 lamdba function.py

---
+++ unit 1/questions 1.2.6 lamdba function.py
@@ -0,0 +1,11 @@
+import functools
+def function(num, item):
+    return num + 1
+password = input("Enter Your password (integers only): ")
+lst = list(map(int, password))
+magic = functools.reduce(function, lst)
+result = (lambda x: x % 4 == 0)(magic)
+if result:
+    print("Correct password!")
+else:
+    print("Wrong password.")
